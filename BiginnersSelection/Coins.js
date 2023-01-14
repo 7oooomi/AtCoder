266a7a86d469b54eae3cc88e4f37ed3fec439719
Naomi Iwamoto <n.20.0612@gmail.com>
2023-01-14 19:13:43 +0900

    del file

---
+++ BiginnersSelection/Coins.js
@@ -0,0 +1,13 @@
+function Main(input) {
+  input = input.split('\n');
+  console.log(input);
+}
+
+let input = `30
+40
+50
+6000
+`;
+Main(input);
+
+// Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
